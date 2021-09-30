c1f8c0ac0824f82d9242c0b943e1074989dabefa
Nadir Jeevanjee <nadirj@princeton.edu>
2021-09-30 08:25:18 -0400

    Final changes from 18co2

--- atm/make_simple_atm.R
+++ atm/make_simple_atm.R
@@ -10,11 +10,12 @@ Ts        = args[1]  # K
 rh        = args[2]
 gamma     = args[3]  # K/km or "m" 
 gamma_st  = args[4]  # K/km, nonnegative!
-co2_ppmv  = args[5]
-Ttp	  = 200   # K
+Ttp       = args[5]
+co2_ppmv  = args[6]
 
 rfmdir	    = "~/18co2/rfm/"
-atmfile     = paste(rfmdir,"atm/Ts",Ts,"_q",co2_ppmv,"_dz100.atm",sep="")
+atmfile     = paste(rfmdir,"atm/Ts",Ts,"_gamma_st",gamma_st,"_rh",rh,"_q",co2_ppmv,".atm",sep="")
+#atmfile     = paste(rfmdir,"atm/Ts",Ts,"_isothermal_q",co2_ppmv,".atm",sep="")
 description = paste("! Simple atmosphere with co2_ppmv = ",co2_ppmv,sep="")
 
 if (file.exists(atmfile)){
@@ -29,16 +30,18 @@ Htemp = 8e3   # crude scale height, km
 #z     = round(z,digits=-1)
 nz    = length(z)
 
-# tabs, qv
-if (gamma != "m"){
-   gamma = 1e-3*gamma  # K/m 
-   tabs  = Ts - gamma*z
-   p     = ps*(tabs/Ts)^(g/gamma/Rd)
-} else if (gamma == "m"){
+# tabs, p, qv
    tabs    = numeric(nz)
    p       = numeric(nz)
    tabs[1] = Ts
    p[1]	   = ps
+if (gamma != "m"){
+   gamma = 1e-3*gamma  # K/m 
+   for (k in 2:nz){
+      tabs[k] = tabs[k-1] - gamma*(diff(z)[k-1])
+      p[k]    = p[k-1]    - g*p[k-1]/Rd/tabs[k-1]*(diff(z)[k-1])
+   }
+} else if (gamma == "m"){
    for (k in 2:nz){
       tabs[k] = tabs[k-1] - gamma_m(tabs[k-1],p[k-1])*(diff(z)[k-1])
       p[k]    = p[k-1]    - g*p[k-1]/Rd/tabs[k-1]*(diff(z)[k-1])
@@ -46,14 +49,16 @@ if (gamma != "m"){
 }   
 qv = rh*qsat(tabs,p)
 
-# add strat
-k_tp  = max(which(tabs>Ttp))+1
-z_tp  = z[k_tp]
-p_tp  = p[k_tp]
-qv_tp = qv[k_tp]
-tabs[(k_tp):nz] = Ttp + 1e-3*gamma_st*(z[k_tp:nz]-z_tp)
-qv[(k_tp):nz]   = qv_tp
-p[(k_tp):nz]    = p_tp*exp(-g/Rd/Ttp*(z[(k_tp):nz]-z_tp))
+# add strat if applicable
+if (Ts > Ttp ){
+   k_tp  = max(which(tabs>Ttp))+1
+   z_tp  = z[k_tp]
+   p_tp  = p[k_tp]
+   qv_tp = qvsat(Ttp,p_tp)
+   tabs[(k_tp):nz] = Ttp + 1e-3*gamma_st*(z[k_tp:nz]-z_tp)
+   qv[(k_tp):nz]   = qv_tp
+   p[(k_tp):nz]    = p_tp*exp(-g/Rd/Ttp*(z[(k_tp):nz]-z_tp))
+}
 n_h20 = qv/m_h2o*m_air
 n_co2 = rep(co2_ppmv*1e-6,times=nz)
 
--- run_rfm.sh
+++ run_rfm.sh
@@ -1,11 +1,26 @@
 # Args
 gas=$1
 atm=$2
-dk=$3   #cm-1
-save_sigma=F
-ctm=ctm
-exec="rfm"
-case=${gas}_${atm}_dk${dk}
+ctm=$3
+dk=$4
+save_sigma=$5
+case=$6
+exec="rfm_new"
+mix=mix
+kmin=500  # cm-1
+kmax=850  # cm-1
+#dk=0.1   # cm-1
+
+if [ $ctm == "noctm" ] ; then
+   ctm=""
+fi
+
+if [ $case == "" ] ; then
+    echo "Case is blank. Exiting."
+    exit
+fi
+
+echo "Begin run_rfm.sh"
 
 # Derived script vars
 atmfile=${atm}.atm
@@ -14,24 +29,24 @@ levfile=z${zlevs}.lev
 
 # Derived config vars
 HDR="$atm atm, $ctm"
-optFLG="flx zen opt vrt $ctm"
-cooFLG="rad flx coo sfc $ctm"
-tabFLG="tab $ctm"
+optFLG="flx zen opt vrt $ctm $mix"
+cooFLG="rad flx coo sfc $ctm $mix"
+tabFLG="tab $ctm $mix"
 ATM="${PWD}/atm/$atmfile"
 LEV="${PWD}/lev/$levfile"
 DIM=PLV
 SFC=$(grep -A 1 "*TEM" atm/${atmfile} | sed -n "2p" | awk '{print($1)}' | rev | cut -c 2- | rev)
 
 if [ $gas = h2o ]; then
-    SPC="10 1500 $dk"
+    SPC="$kmin $kmax $dk"
     GAS="H2O"
     HIT="${PWD}/hit/h2o_1500cm-1.hit"
 elif [ $gas = co2 ]; then
-    SPC="500 850 $dk"
+    SPC="$kmin $kmax $dk"
     GAS=CO2
     HIT="${PWD}/hit/co2_500-850cm-1.hit"
 elif [ $gas = both ]; then
-    SPC="10 1500 1"
+    SPC="$kmin $kmax $dk"
     GAS="H2O CO2"
     HIT="${PWD}/hit/h2o_co2_1500cm-1.hit"
 fi
@@ -39,25 +54,27 @@ HDR="$atm atm, $gas, $ctm"
 
 
 # Directories
-echo "Making directories"
 rfmdir=$PWD
 projectdir=${rfmdir%/rfm}
 project=${projectdir##*/}
 datadir=/tigress/nadirj/${project}_data
 mkdir -p $datadir
+if [ -d $datadir/${case} ] ; then
+    rm -rf $datadir/${case}
+    rm -rf ${case}
+    echo "Removing old case directories"
+fi
+echo "Making directories"
 mkdir -p $datadir/${case}
 ln -s ${datadir}/$case $case 
 for outdir in tab opt coo flx; do 
-    if [ -d ${case}/$outdir ]; then
-	rm ${case}/${outdir}/*
-    elif [ ! -d ${case}/$outdir ]; then
-	mkdir ${case}/${outdir}
-    fi
+    mkdir -p ${case}/${outdir}
 done
 
 # configure and run RFM 
 for field in tab opt coo; do 
 #field=tab  # for debugging 
+    echo "${field} run"
     FLG=${field}FLG
     sed -e "/*HDR/ a\ $HDR"\
 	-e "/*FLG/ a\ ${!FLG}"\
@@ -78,3 +95,5 @@ for field in tab opt coo; do
 done
 
 Rscript asc2ncdf.R $case $gas $save_sigma
+
+echo "END run_rfm.sh"
